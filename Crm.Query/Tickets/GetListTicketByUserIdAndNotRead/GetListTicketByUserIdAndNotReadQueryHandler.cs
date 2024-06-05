fb955745a1315f2fbf1824d63dab63a47f29bbe0
AmirhosseinAbutalebi <Abutalebiamir78@gmail.com>
2024-06-05 16:59:42 +0330

    add dapper in project for child aggregate

--- Crm.Query/Tickets/GetListTicketByUserIdAndNotRead/GetListTicketByUserIdAndNotReadQueryHandler.cs
+++ Crm.Query/Tickets/GetListTicketByUserIdAndNotRead/GetListTicketByUserIdAndNotReadQueryHandler.cs
@@ -1,28 +1,32 @@
-﻿using Crm.Infrastructure.Persistent.Ef;
+﻿using Crm.Infrastructure.Persistent.Dapper;
+using Crm.Infrastructure.Persistent.Ef;
 using Crm.Query.Tickets.DTOs;
+using Dapper;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 namespace Crm.Query.Tickets.GetListTicketByUserIdAndNotRead
 {
     public class GetListTicketByUserIdAndNotReadQueryHandler : IRequestHandler<GetListTicketByUserIdAndNotReadQuery, List<TicketDto>>
     {
         private readonly CrmDbContext _context;
-        public GetListTicketByUserIdAndNotReadQueryHandler(CrmDbContext context)
+        private readonly DapperContext _dapperContext;
+        public GetListTicketByUserIdAndNotReadQueryHandler(CrmDbContext context, DapperContext dapperContext)
         {
             _context = context;
+            _dapperContext = dapperContext;
         }
         public async Task<List<TicketDto>> Handle(GetListTicketByUserIdAndNotReadQuery request, CancellationToken cancellationToken)
         {
             var userName = _context.Users.FirstOrDefault(r => r.Id == request.UserId);
 
-            var tickets = _context.Tickets.FromSqlRaw
-                (@$"Select * from [ticket].[Tickets] where Id in 
+            using var connection = _dapperContext.CreateConnection();
+            var sql = @$"Select * from [ticket].[Tickets] where Id in 
                 (Select TicketsId from [ticket].[TicketDetails] where 
-                TicketReciverId='{userName.Id}' And ReadTicket=0)");
+                TicketReciverId=@id And ReadTicket=0)";
+            var tickets = await connection.QueryAsync<TicketDto>(sql, new { id = userName.Id });
+            var result = tickets.ToList();
 
             var fullName = userName.FirstName + " " + userName.LastName;
 
-            var result = TicketMapper.TicketMapToListDto(tickets.ToList());
             foreach (var item in result)
             {
                 item.FullName = fullName;
--- Crm.Query/Tickets/GetListTicketByUserIdAndRead/GetListTicketByUserIdAndReadQueryHandler.cs
+++ Crm.Query/Tickets/GetListTicketByUserIdAndRead/GetListTicketByUserIdAndReadQueryHandler.cs
@@ -1,28 +1,33 @@
-﻿using Crm.Infrastructure.Persistent.Ef;
+﻿using Crm.Infrastructure.Persistent.Dapper;
+using Crm.Infrastructure.Persistent.Ef;
 using Crm.Query.Tickets.DTOs;
+using Dapper;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 namespace Crm.Query.Tickets.GetListTicketByUserIdAndRead
 {
     public class GetListTicketByUserIdAndReadQueryHandler : IRequestHandler<GetListTicketByUserIdAndReadQuery, List<TicketDto>>
     {
         private readonly CrmDbContext _context;
-        public GetListTicketByUserIdAndReadQueryHandler(CrmDbContext context)
+        private readonly DapperContext _dapperContext;
+        public GetListTicketByUserIdAndReadQueryHandler(CrmDbContext context, DapperContext dapperContext)
         {
             _context = context;
+            _dapperContext = dapperContext;
         }
         public async Task<List<TicketDto>> Handle(GetListTicketByUserIdAndReadQuery request, CancellationToken cancellationToken)
         {
             var userName = _context.Users.FirstOrDefault(r => r.Id == request.UserId);
 
-            var tickets = _context.Tickets.FromSqlRaw
-                (@$"Select * from [ticket].[Tickets] where Id in 
+            using var connection = _dapperContext.CreateConnection();
+            var sql = @$"Select * from [ticket].[Tickets] where Id in 
                 (Select TicketsId from [ticket].[TicketDetails] where 
-                TicketReciverId='{userName.Id}' And ReadTicket=1)");
+                TicketReciverId=@id And ReadTicket=1)";
+
+            var tickets = await connection.QueryAsync<TicketDto>(sql, new { id = userName.Id });
+            var result = tickets.ToList();
 
             var fullName = userName.FirstName + " " + userName.LastName;
 
-            var result = TicketMapper.TicketMapToListDto(tickets.ToList());
             foreach (var item in result)
             {
                 item.FullName = fullName;
